d9c65728cb570b14c3636b41f9863f60fecd30dc
Giuseppe Spataro <spatyx@gmail.com>
2025-04-03 19:18:14 +0200

    update(routes.api): register resources

--- laravel-booking/routes/api.php
+++ laravel-booking/routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CustomerController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::resource('/customer', CustomerController::class);
+Route::resource('/booking', BookingController::class);
