1eaf5a05c8ea7d39c01f387840502db4193f2640
Giuseppe Spataro <spatyx@gmail.com>
2025-04-03 22:25:29 +0200

    feat(auth): implement basic authentication with sanctum

--- laravel-booking/routes/api.php
+++ laravel-booking/routes/api.php
@@ -1,13 +1,13 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookingController;
 use App\Http\Controllers\CustomerController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 
-Route::resource('/customer', CustomerController::class);
-Route::resource('/booking', BookingController::class);
+Route::resource('/customer', CustomerController::class)->middleware('auth:sanctum');
+Route::resource('/booking', BookingController::class)->middleware('auth:sanctum');
