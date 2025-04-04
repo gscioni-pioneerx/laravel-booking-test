04b27c9e2e7a4d9cc33eb45c90f920c0d6c2e2c8
Giuseppe Spataro <spatyx@gmail.com>
2025-04-04 18:08:52 +0200

    new(Seeder): add seeders for customers and bookings

---
+++ laravel-booking/database/seeders/BookingSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Booking;
+use App\Models\Customer;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class BookingSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $customers = Customer::get();
+
+        if (empty($customers)) {
+            return;
+        }
+
+        foreach ($customers as $customer) {
+            Booking::factory()->count(rand(1, 10))->create([
+                'customer_id' => $customer->id
+            ]);
+        }
+    }
+}
