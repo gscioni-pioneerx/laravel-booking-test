dd907ca49ce813a4c8851c95f569493b63d887fa
Giuseppe Spataro <spatyx@gmail.com>
2025-04-04 11:31:16 +0200

    test(BookingController): test store method

--- laravel-booking/tests/Feature/BookingApiTest.php
+++ laravel-booking/tests/Feature/BookingApiTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature;
 
 use App\Models\Booking;
+use App\Models\Customer;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -14,7 +15,7 @@ class BookingApiTest extends TestCase
     use RefreshDatabase;
 
     /**
-     * A basic feature test example.
+     * Test booking index
      */
     public function test_booking_index(): void
     {
@@ -27,5 +28,35 @@ class BookingApiTest extends TestCase
         $response = $this->get('/api/booking');
 
         $response->assertOk()->assertJsonCount(5, 'data');
+    }
+
+    /**
+     * Test booking store
+     */
+    public function test_booking_store(): void
+    {
+        Sanctum::actingAs(
+            User::factory()->create()
+        );
+
+        $customer = Customer::factory()->create();
+
+        $checkInDate = fake()->dateTimeBetween('now', '+3 months');
+        $checkOutDate = (clone $checkInDate)->modify('+' . rand(1, 20) . ' days');
+
+        $bookingData = [
+            'customer_id' => $customer->id,
+            'title' => fake()->text(100),
+            'checkin' => $checkInDate->format('Y-m-d H:i:s'),
+            'checkout' => $checkOutDate->format('Y-m-d H:i:s')
+        ];
+
+        $response = $this->postJson('/api/booking', $bookingData);
+
+        $response->assertStatus(201)->assertJsonFragment([
+            'title' => $bookingData['title'],
+            'checkin' => $bookingData['checkin'],
+            'checkout' => $bookingData['checkout']
+        ]);
     }
 }
